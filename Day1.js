c5bda327d7a7b5958fa0ad35775940bae1d27f08
amit <156525841+ArtistBerg@users.noreply.github.com>
2024-07-16 21:29:33 +0530

    Add files via upload

---
+++ Day1.js
@@ -0,0 +1,55 @@
+// We are gonna learn about variable and data types
+
+// ACTIVITY __[ 1 ]__
+    var num1 = 43;
+    console.log(num1);
+
+    let num2 = 53;
+    console.log(num2);
+
+// ACTIVITY __[ 2 ]__
+    const isRunning = true;
+    console.log(isRunning);
+
+// ACTIVITY __[ 3 ]__
+    let number = 3;
+    let sayMyName = "amit";
+    let isTrue = false;
+    let myObj = {};
+    const numbers = Array(56, 74, 32, 45);
+
+    console.log(typeof(number));
+    console.log(typeof(sayMyName));
+    console.log(typeof(isTrue));
+    console.log(typeof(myObj));
+    console.log(typeof(numbers));
+
+    // OTHER ARRAY
+
+    // String array
+        const courses = Array("javascript", "python");
+
+        // Empty array
+        const empty = Array();
+
+        // Multidimensional array
+        const matrix = Array([
+            [0, 1],
+            [2, 3],
+        ]);
+
+        console.log(numbers);
+        console.log(courses);
+        console.log(empty);
+        console.log(matrix);
+
+// ACTIVITY __[ 4 ]__
+    let year = 2023;
+    console.log(year);
+    year = 2025;
+    console.log(year);
+
+// ACTIVITY __[ 5 ]__
+    const months = 12;
+    months = 14;//observe the error
+
